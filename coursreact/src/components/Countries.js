c5f5238e3b0e6f8de7d3d2874cd2b9b2f220d883
jRayze <merlini.jayson@gmail.com>
2021-09-09 17:39:33 +0200

    cours react world

---
+++ coursreact/src/components/Countries.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Countries = () => {
+    const [data, setData] = useState([])
+
+    return (
+        <div>
+            {data}
+       
+        </div>
+    );
+};
+
+export default Countries;
---
+++ coursreact/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <Logo />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, repudiandae. Esse nulla recusandae hic voluptate maiores doloremque voluptates. Sed fuga numquam deserunt, minima expedita reiciendis iure accusamus temporibus velit eos suscipit aliquam aspernatur veniam reprehenderit dolor, non unde. Inventore maiores voluptatum itaque, nihil vel animi perspiciatis deleniti delectus nulla minus aut recusandae! Maxime, non autem eos perferendis id quaerat obcaecati.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nobis, corporis aliquid totam similique esse maiores ipsam, quisquam eligendi quas vero libero aut voluptatibus cum illo est doloremque? Iste illo pariatur laboriosam sit unde aspernatur, perspiciatis reiciendis rem veniam iusto? Cum corrupti nisi praesentium numquam, in excepturi distinctio vel, doloribus sequi iste alias dolorum neque consequuntur delectus culpa porro. Aperiam nostrum consequatur accusantium vero eius id quidem velit placeat delectus at, eos suscipit nihil, corrupti labore vitae! Laboriosam officia rem soluta assumenda accusamus sed magni esse est dolorum ad! Adipisci incidunt provident sunt quam, quo accusantium. Commodi molestiae deleniti deserunt quae sint quasi dolor asperiores eligendi perferendis, adipisci, qui repudiandae! Aspernatur, cumque alias nemo doloribus expedita rem voluptatibus id molestiae!</p>
+        </div>
+    );
+};
+
+export default About;
